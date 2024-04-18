ed9fc041b196d99835476923d54903520c5356fb
duyviet2101 <117768320+duyviet2101@users.noreply.github.com>
2024-04-18 23:14:57 +0700

    cancel request

--- public/assets/js/contacts.js
+++ public/assets/js/contacts.js
@@ -1,19 +1,21 @@
 //! send request
+const addEventSendRequest = (btn) => {
+  const username = btn.getAttribute('data-username');
+
+  btn.addEventListener('click', () => {
+    socket.emit('CLIENT_SEND_REQUEST_CONTACT', username);
+    btn.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
+          <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
+      </svg>
+    `;
+    btn.disabled = true;
+  });
+};
 const sendRequestBtn = document.querySelectorAll('[send-request]');
 if (sendRequestBtn && sendRequestBtn.length > 0) {
   sendRequestBtn.forEach(btn => {
-    const username = btn.getAttribute('data-username');
-
-    btn.addEventListener('click', () => {
-      socket.emit('CLIENT_SEND_REQUEST_CONTACT', username);
-      btn.innerHTML = `
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-            <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425z" />
-        </svg>
-      `;
-      btn.disabled = true;
-    });
-
+    addEventSendRequest(btn);
   });
 }
 //! end send request
@@ -25,11 +27,16 @@ socket.on('SERVER_RETURN_LENGTH_REQUESTS_RECEIVED', data => {
     lengthContactRequestsReceived
   } = data;
   if (userId == window.user._id) {
-    const countRequest = document.querySelector('.count-request');
+    const countRequestNoti = document.querySelector('.count-request');
 
-    if (countRequest) {
-      countRequest.innerHTML = lengthContactRequestsReceived;
+    if (countRequestNoti) {
+      countRequestNoti.innerHTML = lengthContactRequestsReceived;
       createAlertNotice('Your have a new contact request!')
+    }
+
+    const countRequestPane = document.querySelector('.count-request-pane');
+    if (countRequestPane) {
+      countRequestPane.innerHTML = `(${lengthContactRequestsReceived})`;
     }
   }
 });
@@ -42,12 +49,13 @@ socket.on('SERVER_RETURN_INFO_REQUEST_RECEIVED', data => {
     infoUserA
   } = data;
   if (userId == window.user._id) {
-    const requestList = document.querySelector('.request-list');
-    if (requestList) {
-      requestList.querySelector('.no-noti')?.remove();
-      const firstLi = requestList.querySelector('li');
+    const requestListNoti = document.querySelector('.request-list');
+    if (requestListNoti) {
+      requestListNoti.querySelector('.no-noti')?.remove();
+      const firstLi = requestListNoti.querySelector('li');
 
       const li = document.createElement('li');
+      li.setAttribute('data-username', infoUserA.username);
       li.innerHTML = `
         <div class="tyn-media-group align-items-start">
             <div class="tyn-media tyn-circle">
@@ -85,10 +93,119 @@ socket.on('SERVER_RETURN_INFO_REQUEST_RECEIVED', data => {
         </div>
       `;
       if (firstLi)
-        requestList.insertBefore(li, firstLi);
+        requestListNoti.insertBefore(li, firstLi);
       else
-        requestList.appendChild(li);
+        requestListNoti.appendChild(li);
+    }
+
+    const requestListPane = document.querySelector('#contact-request-received.tab-pane ul');
+    if (requestListPane) {
+      const firstLi = requestListPane.querySelector('li');
+
+      const li = document.createElement('li');
+      li.classList.add('tyn-aside-item', 'js-toggle-main');
+      li.setAttribute('data-username', infoUserA.username);
+      li.innerHTML = `
+        <div class="tyn-media-group">
+          <div class="tyn-media tyn-size-lg">
+            <img src="${ infoUserA.avatar }" alt="">
+          </div>
+          <div class="tyn-media-col" onclick="window.location.href='/contacts/profile/${ infoUserA.username }#contact-request-received'">
+            <div class="tyn-media-row">
+              <h6 class="name">${ infoUserA.fullName }</h6>
+            </div>
+            <div class="tyn-media-row">
+              <p class="content">@${ infoUserA.username }</p>
+            </div>
+          </div>
+          <div class="tyn-media-option tyn-aside-item-option">
+            <ul class="tyn-media-option-list">
+              <li>
+                <button class="btn btn-icon btn-white btn-pill me-1" accept-request data-username="${ infoUserA.username }">
+                  <!-- check2-circle -->
+                  <svg class="bi bi-check2-circle" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0"/>
+                    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0z"/>
+                  </svg>
+                </button>
+                <button class="btn btn-icon btn-white btn-pill" reject-request data-username="${ infoUserA.username }">
+                  <!-- x-lg -->
+                  <svg class="bi bi-x-lg" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                    <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
+                  </svg>
+                </button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      `;
+      if (firstLi)
+        requestListPane.insertBefore(li, firstLi);
+      else
+        requestListPane.appendChild(li);
     }
   };
 });
 //! end SERVER_RETURN_INFO_REQUEST_RECEIVED
+
+//! cancel request
+const addEventCancelRequest = (btn) => {
+  const username = btn.getAttribute('data-username');
+
+  btn.addEventListener('click', () => {
+    socket.emit('CLIENT_CANCEL_REQUEST_CONTACT', username);
+    btn.closest('.tyn-aside-item').remove();
+  });
+};
+const cancelRequestBtn = document.querySelectorAll('[cancel-request]');
+if (cancelRequestBtn && cancelRequestBtn.length > 0) {
+  cancelRequestBtn.forEach(btn => {
+    addEventCancelRequest(btn);
+  });
+};
+//! end cancel request
+
+//! SERVER_RETURN_LENGTH_REQUESTS_RECEIVED_CANCEL
+socket.on('SERVER_RETURN_LENGTH_REQUESTS_RECEIVED_CANCEL', data => {
+  const {
+    userId,
+    lengthContactRequestsReceived
+  } = data;
+  if (userId == window.user._id) {
+    const countRequestNoti = document.querySelector('.count-request');
+
+    if (countRequestNoti) {
+      countRequestNoti.innerHTML = lengthContactRequestsReceived;
+    }
+
+    const countRequestPane = document.querySelector('.count-request-pane');
+    if (countRequestPane) {
+      countRequestPane.innerHTML = `(${lengthContactRequestsReceived})`;
+    }
+  }
+});
+//! end SERVER_RETURN_LENGTH_REQUESTS_RECEIVED_CANCEL
+
+//! SERVER_RETURN_INFO_CANCEL_REQUEST
+socket.on('SERVER_RETURN_INFO_CANCEL_REQUEST', data => {
+  const {
+    userId,
+    username
+  } = data;
+  if (userId == window.user._id) {
+    const requestListNoti = document.querySelector('.request-list');
+    if (requestListNoti) {
+      const li = requestListNoti.querySelector(`li[data-username="${username}"]`);
+      if (li)
+        li.remove();
+    }
+
+    const requestListPane = document.querySelector('#contact-request-received.tab-pane ul');
+    if (requestListPane) {
+      const li = requestListPane.querySelector(`li[data-username="${username}"]`);
+      if (li)
+        li.remove();
+    }
+  }
+});
+//! end SERVER_RETURN_INFO_CANCEL_REQUEST
--- sockets/client/contacts.socket.js
+++ sockets/client/contacts.socket.js
@@ -67,5 +67,47 @@ module.exports = async (req, res) => {
     });
     //! end user send request contact
 
+    //! user cancel request contact
+    socket.on('CLIENT_CANCEL_REQUEST_CONTACT', async (username) => {
+      const userA = await User.findOne({ // current User
+        _id: req.user._id,
+        deleted: false,
+        statusAccount: 'active'
+      });
+
+      const userB = await User.findOne({ // user recive request
+        username: username,
+        deleted: false,
+        statusAccount: 'active',
+      });
+
+      if (!userA || !userB) {
+        return;
+      }
+
+      if (!userA.contactRequestsSent.includes(userB._id) || !userB.contactRequestsReceived.includes(userA._id)) {
+        return;
+      }
+
+      userA.contactRequestsSent = userA.contactRequestsSent.filter(contact => contact.toString() !== userB._id.toString());
+      userB.contactRequestsReceived = userB.contactRequestsReceived.filter(contact => contact.toString() !== userA._id.toString());
+
+      await userA.save();
+      await userB.save();
+
+      //? emit updated length contactRequestsReceived to userB
+      const lengthContactRequestsReceived = userB.contactRequestsReceived.length;
+      socket.broadcast.emit('SERVER_RETURN_LENGTH_REQUESTS_RECEIVED_CANCEL', {
+        userId: userB._id,
+        lengthContactRequestsReceived
+      });
+
+      //? emit info user A to user B
+      socket.broadcast.emit('SERVER_RETURN_INFO_CANCEL_REQUEST', {
+        userId: userB._id,
+        username: userA.username
+      });
+    });
+    //! end user cancel request contact
   });
 }
